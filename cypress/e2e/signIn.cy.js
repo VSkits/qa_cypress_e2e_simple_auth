2e264346f24300ae82cbf02f08659b99d48f4505
VSkits <vityok.nice@gmail.com>
2025-07-15 09:15:30 +0300

    tests for login and logout

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,48 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit(`https://the-internet.herokuapp.com/login`);
+    cy.get('h2')
+      .contains('Login Page');
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type(`SuperSecretPassword!`);
+    cy.get('button')
+      .contains('Login')
+      .click();
+    cy.get('h2')
+      .contains('Secure Area');
+  });
 
+  it('should login with invalid creds', () => {
+    cy.get('#username')
+      .type('to');
+    cy.get('#password')
+      .type(`fhwreif`);
+    cy.get('button')
+      .contains('Login').click();
+    cy.get('#flash')
+      .contains(`Your username is invalid!`);
+  });
+
+  it('should logout from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type(`SuperSecretPassword!`);
+    cy.get('button')
+      .contains('Login')
+      .click();
+    cy.get('h2')
+      .contains('Secure Area');
+    cy.get('i')
+      .contains('Logout')
+      .click();
+    cy.get('h2')
+      .contains('Login Page');
   });
 });
